89f3ca8ac27d41092d472ba4530b297e51b8c179
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-09 16:48:49 +0100

    lista aleatorioa

--- ejers/ejercicio3.py
+++ ejers/ejercicio3.py
@@ -15,3 +15,27 @@ def ejercicio3():
 			gap = n//2
 
 	arr = []
+
+	while True:
+		try:
+			rango_inferior = int(input("Rango Inferior: "))
+			rango_superior = int(input("Rango Superior: "))
+
+			if rango_superior <= 0 or rango_inferior <= 0:
+				print("Sólo se admiten números positivos y mayores que 0. Inténtelo de nuevo")
+			elif rango_superior < rango_inferior:
+				print("el rango inferior debe ser menor que el superior. Inténtelo de nuevo: ")
+			else:
+				break
+
+		except:
+			print("Mensaje incorrecto. Sólo se aceptan números. Inténtelo de nuevo: ")
+
+	longitud = rd(rango_inferior, rango_superior)
+
+	for i in range(longitud):
+		arr.append(rd(0,100))
+	print(arr)
+
+	shellSort(arr)
+	print(arr)
