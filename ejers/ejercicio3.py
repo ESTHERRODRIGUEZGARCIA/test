c474d12d24c153db08a8c3281b33475248d3df3e
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-09 16:43:44 +0100

    tabla

--- ejers/ejercicio3.py
+++ ejers/ejercicio3.py
@@ -4,3 +4,8 @@ def ejercicio3():
     def shellSort(arr):
 		n = len(arr)
 		gap = n//2
+        while gap > 0:
+			for i in range(gap,n):
+                temp = arr[i]
+                j = i
+                
