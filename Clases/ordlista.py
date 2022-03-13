3f913e098942c1d396df9803b93242f4541f9f63
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-13 13:30:24 +0100

    tareas

--- Clases/ordlista.py
+++ Clases/ordlista.py
@@ -1,6 +1,15 @@
 
 def ord(lista):
-    lista = [["a", "d"], ["d", "b"], ["b", "f"],["f", "e"],["e", "c"], ]
+    tareas = {
+    "a": 2,
+    "b": 1,
+    "c": 7,
+    "d": 8,
+    "e": 13,
+    "f": 3
+}
+    lista = [["a", "d"], ["d", "b"], ["b", "f"],["f", "e"],["e", "c"]]
+    ordenada = []
     for i in lista:
         if tareas[i[0]] < tareas[i[1]]:
             ordenada.append(i[0])
