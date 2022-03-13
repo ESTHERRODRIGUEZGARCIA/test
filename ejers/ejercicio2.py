c6ebc191c3c85e5b57bc3f2482119d86b82adafe
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-13 18:07:31 +0100

    shellshort

--- ejers/ejercicio2.py
+++ ejers/ejercicio2.py
@@ -1,25 +1,28 @@
-tareas = {
-    "a": 2,
-    "b": 1,
-    "c": 7,
-    "d": 8,
-    "e": 13,
-    "f": 3
-}
+
+def ejercicio2():
+
+    tareas = {
+        "a": 2,
+        "b": 1,
+        "c": 7,
+        "d": 8,
+        "e": 13,
+        "f": 3
+    }
 
 
-lista = [["a", "d"], ["d", "b"] ,["b", "f"], ["f", "e"] ,["e", "c"]]
-ordenada = []
+    lista = [["a", "d"], ["d", "b"] ,["b", "f"], ["f", "e"] ,["e", "c"]]
+    ordenada = []
 
-def ord(lista):
-    for i in lista:
-        if tareas[i[0]] < tareas[i[1]]:
-            ordenada.append(i[0])
-        else:
-            ordenada.append(i[1])
+    def ord(lista):
+        for i in lista:
+            if tareas[i[0]] < tareas[i[1]]:
+                ordenada.append(i[0])
+            else:
+                ordenada.append(i[1])
 
-ord(lista)
+    ord(lista)
 
-ordenada = list(dict.fromkeys(ordenada))
+    ordenada = list(dict.fromkeys(ordenada))
 
-print(ordenada)
+    print(ordenada)
