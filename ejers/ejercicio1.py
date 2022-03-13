229f50717067f3fa1205ebac5f8933ecffa4eea4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-13 18:56:27 +0100

    aclarando ejer 1

--- ejers/ejercicio1.py
+++ ejers/ejercicio1.py
@@ -1,6 +1,11 @@
 
 def ejercicio1():
-    arr = [5, 7, 3, 2, 10, 9]
+
+    print("La lista que va a ser ordenada es la siguiente: ")
+    arr = [55, 74, 1, 31, 28, 10, 34, 19, 45, 93, 100]
+    print(arr)
+    print("La lista ordenada de mayor a menor es: ")
+    arr = [55, 74, 1, 31, 28, 10, 34, 19, 45, 93, 100]
     for i in range (len(arr)):
         for j in range(len(arr)):
             if arr[i] > arr[j]:
@@ -8,12 +13,15 @@ def ejercicio1():
                 arr[i] = arr[j]
                 arr[j] = aux
     print(arr)
-
-    arr = [5, 7, 3, 2, 10, 9]
+    
+    arr = [55, 74, 1, 31, 28, 10, 34, 19, 45, 93, 100]
     r = []
     for i in range (len(arr)):
         r.append(min(arr))
         arr.remove(min(arr))
+
+    print("La lista ordenada de menor a mayor es: ")            
+
     print(r)
 
 ejercicio1()
