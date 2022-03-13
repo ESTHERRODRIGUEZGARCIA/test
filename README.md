1e3d61c86fa24d237579ab651c4c4ee14814c05f
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-13 18:37:26 +0100

    co

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test
+# Ejercicios de Ordenar
+En este repositorio se encuentran resueltos los ejercicios 1, 2 y 3 
--- ejers/ejercicio3.py
+++ ejers/ejercicio3.py
@@ -2,17 +2,18 @@
 from random import randint as rd
 
 def ejercicio3():
+
     from Clases import shellShort
 
-	while True:
+	while (ejercicio3):
 		try:
 			rango_inferior = int(input("Rango Inferior: "))
 			rango_superior = int(input("Rango Superior: "))
 
 			if rango_superior <= 0 or rango_inferior <= 0:
-				print("Sólo se admiten números positivos y mayores que 0. Inténtelo de nuevo")
+				print("Sólo se admiten números positivos y mayores que 0. Inténtelo de nuevo: ")
 			elif rango_superior < rango_inferior:
-				print("el rango inferior debe ser menor que el superior. Inténtelo de nuevo: ")
+				print("El rango inferior debe ser menor que el superior. Inténtelo de nuevo: ")
 			else:
 				break
 
@@ -20,11 +21,8 @@ def ejercicio3():
 			print("Mensaje incorrecto. Sólo se aceptan números. Inténtelo de nuevo: ")
 
 	longitud = rd(rango_inferior, rango_superior)
-
+	arr = []
 	for i in range(longitud):
 		arr.append(rd(0,100))
 	print(arr)
-
-	print(arr)
-
 
